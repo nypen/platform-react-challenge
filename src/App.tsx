38123b6cc0c0f44483dc131fe797b5e9a449bda1
penny papadimas <pennypapadimas@gmail.com>
2025-03-28 12:45:05 +0200

    Wrap root component with query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,24 @@ import './App.css'
 import { AppRoute } from './routes'
 import { PageLayout } from './components/common/pageLayout'
 import { CatsPage } from './components/pages/cats/catsPage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({});
 
 function App() {
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<PageLayout />}>
-                    <Route path={AppRoute.Cats} element={<CatsPage />} />
-                    <Route path={AppRoute.Breeds} element={'Breeds'} />
-                    <Route path={AppRoute.Favourites} element={'Favourites'} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<PageLayout />}>
+                        <Route path={AppRoute.Cats} element={<CatsPage />} />
+                        <Route path={AppRoute.Breeds} element={'Breeds'} />
+                        <Route path={AppRoute.Favourites} element={'Favourites'} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     )
 }
 
