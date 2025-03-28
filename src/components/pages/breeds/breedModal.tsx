f8b36a62b4b01aa96e262d5834a0d16628390f58
penny papadimas <pennypapadimas@gmail.com>
2025-03-28 14:34:59 +0200

    Streamline error and loading states in breed modal

--- src/components/pages/breeds/breedModal.tsx
+++ src/components/pages/breeds/breedModal.tsx
@@ -4,36 +4,36 @@ import { ImageCard } from '../../common/imageCard';
 import { CardsList } from '../../common/cardsList';
 import { Loading } from '../../core/loading';
 import { Modal } from '../../core/modal';
+import { Typography } from '../../core/typography';
 
 interface BreedModalProps {
 	id: string;
 	onClose: () => void;
 }
+
 const BreedModal = ({ id, onClose }: BreedModalProps) => {
-	const { data: breed } = useBreed(id);
+	const { data: breed, isLoading, isError, error } = useBreed(id);
 	const { data: catImages, isLoading: isCatImagesLoading } = useCatImages({
 		breedIds: breed ? [breed?.id] : [],
 		options: { enabled: !!id },
 	});
 
 	const getContent = () => {
-		if (!breed) {
-			return <div>No breed found</div>;
+		if (isCatImagesLoading || isLoading) {
+			return <Loading variant='dots' />;
 		}
 
-		if (isCatImagesLoading) {
-			return <Loading variant='dots' />;
+		if (isError) {
+			return <Typography variant='error'>No breed found: {error.message}</Typography>;
 		}
 
 		return (
 			<>
-				<div>{breed?.description}</div>
-				<div>
-					<CardsList
-						items={catImages || []}
-						renderCard={(item) => <ImageCard key={item.id} imageUrl={item.imageUrl} />}
-					/>
-				</div>
+				<Typography>{breed?.description}</Typography>
+				<CardsList
+					items={catImages || []}
+					renderCard={(item) => <ImageCard key={item.id} imageUrl={item.imageUrl} />}
+				/>
 			</>
 		);
 	};
